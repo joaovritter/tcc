cfb7a968f4c6e792ff588255a03fb331c0c757e8
joaovritter <joao.ritter@ufn.edu.br>
2026-08-12 15:04:46 -0300

    front end: funcao para api fetch

---
+++ client/src/services/api.ts
@@ -0,0 +1,38 @@
+
+
+const API_URL = 'http://localhost:3000';
+
+
+/**
+ * Estende as opções de configurações padrão do fetch (method, headers, etc.)
+ * e flexibiliza o 'body' para aceitar objetos JS antes da conversão para JSON.
+ */
+interface OpcoesFetch extends RequestInit {
+    body?: any;
+}
+
+//funcao que monta url, injeta o token quando existe, e transforma resposta de erro numa exceção
+// ...(spread) tira a embalagem de objeto ou lista e despeja só o conteudo.
+
+async function apiFetch(caminho: string, opcoes: OpcoesFetch = {}) {
+    const token = localStorage.getItem('token');
+
+    //junta url, injeta cabeçalho de autorizacao e converte o corpo da requisicao
+    const resposta = await fetch(`${API_URL}${caminho}`, {
+        ...opcoes, //repassa qualquer propriedade recebida em opcoes
+        headers: {
+            'Content-Type': 'application/json',
+            ...(token ? { Authorization: `Bearer ${token}` } : {}), //se token existe no localStorage: adiciona o cabecalho, se não: um objeto vazio {} (nao quebra sintaxe)
+            ...opcoes.headers,
+        },
+        body: opcoes.body ? JSON.stringify(opcoes.body) : undefined,
+    })
+    const dados = await resposta.json();
+
+    if(!resposta.ok){
+        throw new Error (dados.erro ?? 'Erro na requisição');
+    }
+
+    return dados;
+
+}
